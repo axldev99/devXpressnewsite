694a1cfa56058c6c997e7daa6765e2bddef8b44a
Laurent Généreux <laurenne.ge@gmail.com>
2022-11-04 22:15:20 -0400

    das

--- components/navbar.php
+++ components/navbar.php
@@ -1,14 +1,25 @@
-    <nav class="flex desk" style="position:sticky !important; top:0px;">
+    <nav class="flex" style="position:sticky !important; top:0px;">
+
+      <img style="height: 75px;" src="img/Social_Profile_Mirko_1-removebg-preview.png">
+
       <div id="link-wrap" class="link-wrap">
-        <a href="#topPage" onclick="closeMenu()" class="navBar-Link disapear"><div class="active intraLink" id="home">Accueil</div></a>
-        <a href="#aPropos" onclick="closeMenu()" class="navBar-Link disapear"><div class="intraLink" id="about">À propos</div></a>
-        <a href="#mesProjets" onclick="closeMenu()" class="navBar-Link disapear"><div class="intraLink" id="projects">Projets</div></a>
-        <a href="#contactMe" onclick="closeMenu()" class="navBar-Link disapear"><div class="intraLink" id="contact">Contact</div></a>
+        <a href="#topPage" onclick="closeMenu()" class="navBar-Link disapear">
+          <div class="active intraLink" id="home">Accueil</div>
+        </a>
+        <a href="#aPropos" onclick="closeMenu()" class="navBar-Link disapear">
+          <div class="intraLink" id="about">À propos</div>
+        </a>
+        <a href="#mesProjets" onclick="closeMenu()" class="navBar-Link disapear">
+          <div class="intraLink" id="projects">Projets</div>
+        </a>
+        <a href="#contactMe" onclick="closeMenu()" class="navBar-Link disapear">
+          <div class="intraLink" id="contact">Contact</div>
+        </a>
 
       </div>
       <a onclick="openMenu()" class="icon navBar-Link navbarHamburger">
         <i class="fa fa-bars"></i>
       </a>
-    </nav>  
+    </nav>
 
     <script src="js/navbar.js"></script>
