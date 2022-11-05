aac550d32e076e4748c2365a2b595daeb5323032
axldev99 <aarseneault@escalademarketing.com>
2022-11-05 16:41:13 -0400

    Carousel marche, a adapter pour PC mobile Tablette

---
+++ components/carousel.php
@@ -0,0 +1,47 @@
+<div class="carouselContainer">
+        <div class="img-slider">
+      <div class="slide active">
+        <img src="1.jpg" alt="">
+        <div class="info">
+          <h2>Slide 01</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="2.jpg" alt="">
+        <div class="info">
+          <h2>Slide 02</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="3.jpg" alt="">
+        <div class="info">
+          <h2>Slide 03</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="4.jpg" alt="">
+        <div class="info">
+          <h2>Slide 04</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="5.jpg" alt="">
+        <div class="info">
+          <h2>Slide 05</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="btn active"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+    </div>
+</div>
+<script src="js/carousel.js"></script>
--- index.php
+++ index.php
@@ -2,28 +2,38 @@
     <head>
         <link rel="stylesheet" href="assets/css/main.css">
         <link rel="stylesheet" href="assets/css/navbar.css">
+        <link rel="stylesheet" href="assets/css/carousel.css">
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
     </head>
+
     <body class="no-scroll transparentScroll">
         <?php include "components/loader.php"; ?>
-
         <div style="overflow:unset; top:0px;">
-            <?php include "components/navbar.php"; ?>
+            
+        <?php include "components/navbar.php"; ?>
             
             <section id="landingSection" class="landingSection">
+                
                 <div class="landing-top-part">
                     <div class="top-logo">
                         <img class="top-logo-img" src="assets/images/logo/Social_Profile_Mirko_1.png">
                     </div>
                     <h1>DevXpress | Bien plus qu'une agence web</h1>
-                    <a class="left-submission-btn special-btn" href="#soumission">Soumission</a><a class="right-portfolio-btn special-btn" href="portfolio.php">Portfolio</a>
+                    
+                    <?php include "components/carousel.php"?>
+                    
+                    <div class="btn-layout">
+                        <a class="left-submission-btn special-btn" href="#soumission">Soumission</a>
+                        <a class="right-portfolio-btn special-btn" href="portfolio.php">Portfolio</a>
+                    </div>
                     <a href="#businessSection"><i class="fa fa-arrow-down bottom-arrow-jump jumping-animation" aria-hidden="true"></i></a>
                 </div>
             </section>
         
+
             <section style="height:2000px"id="businessSection" class="businessSection">
                 <div class="piedestal">
                     <div class="mobile-version floating-animation"></div>
